cc56c80b69c3ed28c581d7b4919382d1a54b85cc
datpham0412 <tiendat041202@gmail.com>
2024-11-22 14:15:26 +1100

    feat: adding Toaster notification to the root layout of the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
 
 export const metadata = {
   title: "Breadit",
@@ -28,6 +29,7 @@ export default function RootLayout({
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
         </div>
+        <Toaster />
       </body>
     </html>
   );
