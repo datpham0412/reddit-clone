417ca88a7b0179a0b7bb3899fe91569c822cfd94
datpham0412 <tiendat041202@gmail.com>
2024-11-25 16:03:46 +1100

    feat: add Providers component to the root layout of the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
-import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/Toaster";
+
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Breadit",
   description: "A Reddit clone built with Next.js and TypeScript.",
 };
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
   authModal,
 }: {
@@ -27,14 +29,15 @@ export default async function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {/* @ts-expect-error server component */}
-        <Navbar />
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+          {authModal}
 
-        {authModal}
-
-        <div className="container max-w-7xl mx-auto h-full pt-12">
-          {children}
-        </div>
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+        </Providers>
         <Toaster />
       </body>
     </html>
