77628137f785ee3a700ac0c2db5564228e570833
datpham0412 <tiendat041202@gmail.com>
2024-11-25 11:25:04 +1100

    feat(UI): implement close modal icon - functionality

---
+++ src/components/CloseModal.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/Button";
+import { X } from "lucide-react";
+
+const CloseModal = () => {
+  const router = useRouter();
+  return (
+    <Button
+      variant="subtle"
+      className="h-6 w-6 p-0 rounded-md"
+      onClick={() => router.back()}
+      aria-label="close-modal"
+    >
+      <X />
+    </Button>
+  );
+};
+
+export default CloseModal;
