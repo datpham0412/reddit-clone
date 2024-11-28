96b20be241465a7b4640d0117a333f98fd902314
datpham0412 <tiendat041202@gmail.com>
2024-11-28 17:05:11 +1100

    feat(types): add ExtendedPost type for enriched post data structure

---
+++ src/types/db.d.ts
@@ -0,0 +1,8 @@
+import { Post, Subreddit, User } from "@prisma/client";
+
+export type ExtendedPost = Post & {
+    subreddit: Subreddit,
+    votes: Vote[],
+    author: User,
+    comments: Comment[]
+}
